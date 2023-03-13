fdfc0fbb56c13f530962efe6635acee1a0b9ae81
Camia Trefs Sebastian <logicalbrainstudio@gmail.com>
2023-03-13 19:45:39 -0300

    Adding NaN <= 0 conditionals and while loop

--- calcfinanciera.js
+++ calcfinanciera.js
@@ -1,23 +1,33 @@
-let capital = prompt("Cuanto dinero quieres depositar?")
-let dias = prompt("Durante cuantos dias te gustaria depositar?")
-
 // Configuramos la TNA
 const tna = 0.75;
 
-// Convertimos la TNA en TEA con n capitalizaciones dependiendo de la cantidad de dias que deposite.
-const n = 360 / dias;
-const tea = ((1 + tna / n) ** n) - 1;
+// Formula de interes compuesto
+function compuesto(capital, dias, tea) {
+    monto = capital * ((1 + tea) ** (dias / 365))
+    return monto
+}
 
 // Verificamos que los prompts sean numeros mayores a cero
-if (capital > 0 && dias > 0) {
-    function compuesto(capital, dias, tea) {
-        // Formula de interes compuesto
-        monto = capital * ((1 + tea) ** (dias / 365))
-        return monto
+validacion = true
+
+while (validacion) {
+    let capital = prompt("Cuanto dinero quieres depositar?")
+    if (isNaN(capital) || capital <= 0) {
+        alert("Por favor ingresa un numero mayor a cero.")
+        validacion = true
+    }
+    else {
+        let dias = prompt("Durante cuantos dias te gustaria depositar?")
+        if (isNaN(dias) || dias <= 0) {
+            alert("Por favor ingresa un numero mayor a cero.")
+            validacion = true
+        }
+        else {
+            // Convertimos la TNA en TEA con n capitalizaciones dependiendo de la cantidad de dias que deposite.
+            const n = 360 / dias;
+            const tea = ((1 + tna / n) ** n) - 1;
+            alert("Depositando $" + capital + ", obtendras $" + compuesto(capital, dias, tea).toFixed(2) + " al final del periodo de " + dias + " dias. De ese monto, recibiras $" + (monto - capital).toFixed(2) + " en concepto de interes.")
+            validacion = false
+        }
     }
 }
-else {
-    alert("Por favor ingrese un numero mayor a cero.")
-}
-
-alert("Depositando $" + capital + ", obtendras $" + compuesto(capital, dias, tea).toFixed(2) + " al final del periodo de " + dias + " dias. De ese monto, recibiras $" + (monto - capital).toFixed(2) + " en concepto de interes.")
