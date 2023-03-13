c7a9f95be701beb13ca9ef51b4d81561f073b2c5
Camia Trefs Sebastian <logicalbrainstudio@gmail.com>
2023-03-13 20:39:15 -0300

    Adding thousands separators

--- calcfinanciera.js
+++ calcfinanciera.js
@@ -1,23 +1,26 @@
 // Configuramos la TNA
 const tna = 0.75;
 
+// Definimos la region para determinar los separadores de miles
+region = "en-GB"
+
 // Formula de interes compuesto
 function compuesto(capital, dias, tea) {
     monto = capital * ((1 + tea) ** (dias / 365))
-    return monto
+    return parseFloat(monto)
 }
 
 // Verificamos que los prompts sean numeros mayores a cero
 validacion = true
 
 while (validacion) {
-    let capital = prompt("Cuanto dinero quieres depositar?")
+    let capital = parseFloat(prompt("Cuanto dinero quieres depositar?"))
     if (isNaN(capital) || capital <= 0) {
         alert("Por favor ingresa un numero mayor a cero.")
         validacion = true
     }
     else {
-        let dias = prompt("Durante cuantos dias te gustaria depositar?")
+        let dias = parseFloat(prompt("Durante cuantos dias te gustaria depositar?"))
         if (isNaN(dias) || dias <= 0) {
             alert("Por favor ingresa un numero mayor a cero.")
             validacion = true
@@ -26,7 +29,7 @@ while (validacion) {
             // Convertimos la TNA en TEA con n capitalizaciones dependiendo de la cantidad de dias que deposite.
             const n = 360 / dias;
             const tea = ((1 + tna / n) ** n) - 1;
-            alert("Depositando $" + capital + ", obtendras $" + compuesto(capital, dias, tea).toFixed(2) + " al final del periodo de " + dias + " dias. De ese monto, recibiras $" + (monto - capital).toFixed(2) + " en concepto de interes.")
+            alert("Depositando $" + capital.toLocaleString(region) + ", obtendras $" + parseFloat((compuesto(capital, dias, tea)).toFixed(2)).toLocaleString(region) + " al final del periodo de " + dias + " dias. De ese monto, recibiras $" + (monto.toFixed(2) - capital.toFixed(2)).toLocaleString(region) + " en concepto de interes.")
             validacion = false
         }
     }
