91019bc4e77ddbb3d2575becb16434129727b57c
Camia Trefs Sebastian <logicalbrainstudio@gmail.com>
2023-03-15 10:02:53 -0300

    Adding emojis to prompts&alerts

--- calcfinanciera.js
+++ calcfinanciera.js
@@ -14,22 +14,22 @@ function compuesto(capital, dias, tea) {
 validacion = true
 
 while (validacion) {
-    let capital = parseFloat(prompt("Cuanto dinero quieres depositar?"))
+    let capital = parseFloat(prompt("🏦 Cuanto dinero quieres depositar?"))
     if (isNaN(capital) || capital <= 0) {
-        alert("Por favor ingresa un numero mayor a cero.")
+        alert("❌ Por favor ingresa un numero mayor a cero.")
         validacion = true
     }
     else {
-        let dias = parseFloat(prompt("Durante cuantos dias te gustaria depositar?"))
+        let dias = parseFloat(prompt("⌛ Durante cuantos dias te gustaria depositar?"))
         if (isNaN(dias) || dias <= 0) {
-            alert("Por favor ingresa un numero mayor a cero.")
+            alert("❌ Por favor ingresa un numero mayor a cero.")
             validacion = true
         }
         else {
             // Convertimos la TNA en TEA con n capitalizaciones dependiendo de la cantidad de dias que deposite.
             const n = 360 / dias;
             const tea = ((1 + tna / n) ** n) - 1;
-            alert("Depositando $" + capital.toLocaleString(region) + ", obtendras $" + parseFloat((compuesto(capital, dias, tea)).toFixed(2)).toLocaleString(region) + " al final del periodo de " + dias + " dias. De ese monto, recibiras $" + (monto.toFixed(2) - capital.toFixed(2)).toLocaleString(region) + " en concepto de interes.")
+            alert("💹 Depositando $" + capital.toLocaleString(region) + ", obtendras $" + parseFloat((compuesto(capital, dias, tea)).toFixed(2)).toLocaleString(region) + " al final del periodo de " + dias + " dias. De ese monto, recibiras $" + (monto.toFixed(2) - capital.toFixed(2)).toLocaleString(region) + " en concepto de interes.")
             validacion = false
         }
     }
