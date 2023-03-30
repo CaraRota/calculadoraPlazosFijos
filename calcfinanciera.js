615d3fd839d907e34663641e9299b565c38a4494
Camia Trefs Sebastian <logicalbrainstudio@gmail.com>
2023-03-30 09:37:32 -0300

    array as const and minor tweaks

--- calcfinanciera.js
+++ calcfinanciera.js
@@ -5,23 +5,25 @@ const tna = 0.78
 const region = "es-AR"
 
 // Formula de interes compuesto
-function compuesto(capital, dias, tea) {
+const calcularInteresCompuesto = (capital, dias, tea)=> {
     monto = capital * ((1 + tea) ** (dias / 365))
     return monto
 }
 
 // Definimos un constructor para determinar los valores a almacenar en el programa
 class historialCalculadora {
-    constructor(nombre, monto, intereses, plazo) {
-        this.nombre = nombre;
-        this.monto = monto;
-        this.intereses = intereses;
-        this.plazo = plazo;
+    constructor(nombre, capital, monto, intereses, plazo, renovacion) {
+        this.nombre = nombre
+        this.capital = capital
+        this.monto = monto
+        this.intereses = intereses
+        this.plazo = plazo
+        this.renovacion = renovacion
     }
 }
 
 // Definimos un array vacio que almacenara los datos del historial de calculos realizado por el cliente
-let historialCalculadoraArray = []
+const historialCalculadoraArray = []
 
 // Verificamos que los prompts sean numeros mayores a cero
 let validacion = true
@@ -29,7 +31,7 @@ let validacion1 = true
 let validacion2 = true
 
 // Funcion que nos permite cargar datos a traves de prompts en un array
-function get_data() {
+const obtenerDatos = ()=> {
     while (validacion) {
         let nombre = prompt("Cual es su gracia?").trim().toUpperCase()
         if (nombre.length < 3) {
@@ -51,13 +53,14 @@ function get_data() {
                             // Convertimos la TNA en TEA con n capitalizaciones dependiendo de la cantidad de dias que deposite.
                             const n = 360 / dias
                             const tea = ((1 + tna / n) ** n) - 1
-                            let monto1 = parseFloat((compuesto(capital, dias, tea)).toFixed(2))
+                            let monto1 = parseFloat((calcularInteresCompuesto(capital, dias, tea)).toFixed(2))
                             let intereses = (monto1 - capital).toLocaleString(region)
+                            let renovacion = new Date((Date.now() + dias)).toLocaleString(region)
 
-                            alert("💹 Depositando $" + capital.toLocaleString(region) + ", obtendras $" + monto1.toLocaleString(region) + " al final del periodo de " + dias + " dias. De ese monto, recibiras $" + intereses + " en concepto de interes.")
+                            alert("💹 Depositando $" + capital.toLocaleString(region) + ", obtendras $" + monto1.toLocaleString(region) + " al final del periodo de " + dias + " dias. De ese monto, recibiras $" + intereses + " en concepto de interes. Podras renovar este plazo fijo el dia "+renovacion)
 
                             // Declaramos una variable que nos permita agregar los datos ingresados por el usuario
-                            const historial = new historialCalculadora(nombre, monto1, intereses, dias)
+                            const historial = new historialCalculadora(nombre, capital, monto1, intereses, dias, renovacion)
                             historialCalculadoraArray.push(historial)
                             // Tiramos un console.table para que se nos muestre el Array bonito por consola
                             console.table(historialCalculadoraArray)
@@ -76,10 +79,10 @@ function get_data() {
     validacion2 = true
 }
 // Funcion que nos permite eliminar el historial de calculos realizado por los usuarios
-function clear_array() {
+const borarArray = ()=> {
     // Chequeamos que el historial de calculos tenga datos cargados
     if (historialCalculadoraArray.length > 0) {
-        historialCalculadoraArray = []
+        historialCalculadoraArray.length = 0
         alert("🆑 Su historial de calculos ha sido borrado")
         // Chequeamos que el hitorial se haya borrado mediante con console.log (eliminar estas lineas una vez pasado el QA)
         console.log(historialCalculadoraArray)
