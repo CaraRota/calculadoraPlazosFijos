a247d96658dfa450a4b30492e57a43ccc312ce44
Camia Trefs Sebastian <logicalbrainstudio@gmail.com>
2023-04-30 11:20:26 -0300

    Autorefresh updated+comments added

--- js/main.js
+++ js/main.js
@@ -61,11 +61,8 @@ const borrarArray = () => {
 
 // Llamamos a la funcion consultas mediante el boton de Consultas
 const misConsultas = () => {
-    if (localStorage.length > 0) {
-        agregarConsultas() //En DOM.js
-    } else {
-        alertas("error", "Error!", "Usted aun no ha realizado ninguna consulta.", "animate__animated animate__headShake")
-    }
+    // Ejemplo de operador ternario
+    localStorage.length > 0 ? agregarConsultas() : alertas("error", "Error!", "Usted aun no ha realizado ninguna consulta.", "animate__animated animate__headShake")
 }
 
 // // Llamamos a la funcion borrarConsultas mediante el boton de Borrar Consultas
@@ -99,3 +96,85 @@ const borrarConsultas = () => {
         alertas("error", "Error!", "Usted aun no tiene ninguna consulta archivada.", "animate__animated animate__headShake")
     }
 }
+
+// COMIENZO SECCION FETCH: CAROUSEL CON COTIZACIONES EN TIEMPO REAL DEL TIPO DE CAMBIO UTILIZANDO APIs DE AMBITO.COM
+
+const cotizacionesGenerales = () => {
+    fetch(apiUrl)
+        .then((response) => response.json())
+        .then((data) => {
+            const oficial = document.querySelector("#dolarOficial")
+            oficial.innerHTML = `${data[0].nombre}: $${data[0].venta}`
+
+            const informal = document.querySelector("#dolarInformal")
+            informal.innerHTML = `${data[1].nombre}: $${data[1].venta}`
+
+            const turista = document.querySelector("#dolarTurista")
+            turista.innerHTML = `${data[2].nombre}: $${data[2].venta}`
+
+            const ahorro = document.querySelector("#dolarAhorro")
+            ahorro.innerHTML = `${data[3].nombre}: $${data[3].venta}`
+
+            const mayorista = document.querySelector("#dolarMayorista")
+            mayorista.innerHTML = `${data[4].nombre}: $${data[4].venta}`
+        })
+}
+
+const cotizacionesMep = () => {
+    fetch(apiUrl2)
+        .then((response) => response.json())
+        .then((data) => {
+            const ccl = document.querySelector("#dolarCcl")
+            ccl.innerHTML = `Dolar CCL: $${data.venta}`
+        })
+}
+
+const cotizacionesCcl = () => {
+    fetch(apiUrl3)
+        .then((response) => response.json())
+        .then((data) => {
+            const mep = document.querySelector("#dolarMep")
+            mep.innerHTML = `Dolar MEP: ${data.venta}`
+        })
+}
+
+// Utilizamos la API general de ambito.com (esta API no incluye CCL y MEP, estos datos deberemos obtenerlos de otras urls)
+const apiUrl = "https://mercados.ambito.com/home/general"
+
+// Creamos un infinite loop con setTimeout y llamando a una funcion que llame a cotizacionesGenerales() para que se actualice la cotizacion cada refreshTimeout segundos
+
+const refreshTimeout = 60000 //Actualizamos cada 60s
+
+// Funcion "loop refresh"
+const actualizarCotizaciones = () => {
+    cotizacionesGenerales()
+    setTimeout(() => {
+        actualizarCotizaciones()
+    }, refreshTimeout)
+}
+
+actualizarCotizaciones()
+
+// Utilizamos otra API para atrapar el dolar CCL
+const apiUrl2 = "https://mercados.ambito.com//dolarrava/cl/variacion"
+
+const actualizarCotizacionesMep = () => {
+    cotizacionesMep()
+    setTimeout(() => {
+        actualizarCotizacionesMep()
+    }, refreshTimeout)
+}
+actualizarCotizacionesMep()
+
+// Utilizamos otra API para atrapar el dolar MEP
+const apiUrl3 = "https://mercados.ambito.com//dolarrava/mep/variacion"
+
+const actualizarCotizacionesCcl = () => {
+    cotizacionesCcl()
+    setTimeout(() => {
+        actualizarCotizacionesCcl()
+    }, refreshTimeout)
+}
+actualizarCotizacionesCcl()
+
+// FIN SECCION FETCH: CAROUSEL CON COTIZACIONES EN TIEMPO REAL DEL TIPO DE CAMBIO UTILIZANDO APIs DE AMBITO.COM
--- js/variables.js
+++ js/variables.js
@@ -1,4 +1,4 @@
-// Configuramos la TNA
+// Configuramos la TNA (deberiamos buscar alguna APi para actualizar este valor automaticamente)
 const tna = 0.91
 
 // Definimos la region para determinar los separadores de miles
@@ -20,77 +20,3 @@ const alertas = (icon, titulo, texto, animation) => {
         buttonsStyling: false
     })
 }
-
-// Utilizamos la API general de ambito.com (esta API no incluye CCL y MEP)
-const apiUrl = "https://mercados.ambito.com/home/general"
-
-// Creamos un infinite loop con setTimeout y llamando luego a la funcion para que se actualice la cotizacion cada x segundos
-
-const cotizacionesGenerales = () => {
-    fetch(apiUrl)
-        .then((response) => response.json())
-        .then((data) => {
-            const oficial = document.querySelector("#dolarOficial")
-            oficial.innerHTML = `${data[0].nombre}: $${data[0].venta}`
-
-            const informal = document.querySelector("#dolarInformal")
-            informal.innerHTML = `${data[1].nombre}: $${data[1].venta}`
-
-            const turista = document.querySelector("#dolarTurista")
-            turista.innerHTML = `${data[2].nombre}: $${data[2].venta}`
-
-            const ahorro = document.querySelector("#dolarAhorro")
-            ahorro.innerHTML = `${data[3].nombre}: $${data[3].venta}`
-
-            const mayorista = document.querySelector("#dolarMayorista")
-            mayorista.innerHTML = `${data[4].nombre}: $${data[4].venta}`
-        })
-}
-
-const actualizarCotizaciones = () => {
-    cotizacionesGenerales()
-    setTimeout(() => {
-        actualizarCotizaciones()
-    }, 5000)
-}
-actualizarCotizaciones()
-
-// Utilizamos otra API para atrapar el dolar CCL
-const apiUrl2 = "https://mercados.ambito.com//dolarrava/cl/variacion"
-
-const cotizacionesMep = () => {
-    fetch(apiUrl2)
-        .then((response) => response.json())
-        .then((data) => {
-            const ccl = document.querySelector("#dolarCcl")
-            ccl.innerHTML = `Dolar CCL: $${data.venta}`
-        })
-}
-
-const actualizarCotizacionesMep = () => {
-    cotizacionesMep()
-    setTimeout(() => {
-        actualizarCotizacionesMep()
-    }, 5000)
-}
-actualizarCotizacionesMep()
-
-// Utilizamos otra API para atrapar el dolar MEP
-const apiUrl3 = "https://mercados.ambito.com//dolarrava/mep/variacion"
-
-const cotizacionesCcl = () => {
-    fetch(apiUrl3)
-        .then((response) => response.json())
-        .then((data) => {
-            const mep = document.querySelector("#dolarMep")
-            mep.innerHTML = `Dolar MEP: ${data.venta}`
-        })
-}
-
-const actualizarCotizacionesCcl = () => {
-    cotizacionesCcl()
-    setTimeout(() => {
-        actualizarCotizacionesCcl()
-    }, 5000)
-}
-actualizarCotizacionesCcl()
